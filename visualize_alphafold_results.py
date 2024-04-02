fc6de759431064e15f80def6b9e1d89cab6c2edc
Chris Field <94464835+crfield18@users.noreply.github.com>
2024-04-02 15:35:21 +0100

    figure labelling and formatting changes to fit personal preference

--- visualize_alphafold_results.py
+++ visualize_alphafold_results.py
@@ -1,10 +1,11 @@
 import glob
 import math
 import os
-import numpy as np
-from matplotlib import pyplot as plt
 import argparse
 import pickle
+import json
+import numpy as np
+from matplotlib import pyplot as plt
 
 def get_pae_plddt(model_names):
     out = {}
@@ -15,6 +16,28 @@ def get_pae_plddt(model_names):
         out[f'{basename}'] = {'plddt': d['plddt'], 'pae':d['predicted_aligned_error']}
     return out
 
+def get_ranking():
+    with open(f'{args.input_dir}/ranking_debug.json', 'r') as ranking_file:
+        ranking_json_data = json.load(ranking_file)
+
+    ranking_lut = {
+        ranking_json_data['order'][0]: 'ranked_0',
+        ranking_json_data['order'][1]: 'ranked_1',
+        ranking_json_data['order'][2]: 'ranked_2',
+        ranking_json_data['order'][3]: 'ranked_3',
+        ranking_json_data['order'][4]: 'ranked_4'
+        }
+
+    plddt_lut = {
+        'ranked_0': f"{ranking_json_data['plddts'][ranking_json_data['order'][0]]:.2f}",
+        'ranked_1': f"{ranking_json_data['plddts'][ranking_json_data['order'][1]]:.2f}",
+        'ranked_2': f"{ranking_json_data['plddts'][ranking_json_data['order'][2]]:.2f}",
+        'ranked_3': f"{ranking_json_data['plddts'][ranking_json_data['order'][3]]:.2f}",
+        'ranked_4': f"{ranking_json_data['plddts'][ranking_json_data['order'][4]]:.2f}"
+    }
+
+    return ranking_lut, plddt_lut
+
 def generate_output_images(feature_dict, out_dir, name, pae_plddt_per_model):
     msa = feature_dict['msa']
     seqid = (np.array(msa[0] == msa).mean(-1))
@@ -24,7 +47,7 @@ def generate_output_images(feature_dict, out_dir, name, pae_plddt_per_model):
     final = non_gaps[seqid_sort] * seqid[seqid_sort, None]
 
     ##################################################################
-    plt.figure(figsize=(14, 4), dpi=100)
+    plt.figure(figsize=(14, 4), dpi=300)
     ##################################################################
     plt.subplot(1, 2, 1)
     plt.title("Sequence coverage")
@@ -39,10 +62,34 @@ def generate_output_images(feature_dict, out_dir, name, pae_plddt_per_model):
     plt.ylabel("Sequences")
 
     ##################################################################
+
+    # Set colours for each line based on pLDDT ranking
+    legend_colour_lut = {
+        'ranked_0': 'C0',
+        'ranked_1': 'C1',
+        'ranked_2': 'C2',
+        'ranked_3': 'C3',
+        'ranked_4': 'C4'
+    }
+
     plt.subplot(1, 2, 2)
     plt.title("Predicted LDDT per position")
+
+    json_rankings, avg_plddts = get_ranking()
+
     for model_name, value in pae_plddt_per_model.items():
-        plt.plot(value["plddt"], label=model_name)
+        ranked_file_name = json_rankings[model_name.replace('.pkl', '')]
+        plt.plot(value["plddt"],
+                 label=f"{ranked_file_name} ({model_name.replace('_ptm.pkl', '')}; \u0078\u0304={avg_plddts[ranked_file_name]})",
+                 color=legend_colour_lut[ranked_file_name])
+    plt.legend()
+
+    # Sort figure legend labels alphabetically
+    handles, labels = plt.gca().get_legend_handles_labels()
+    handles_labels_sorted = sorted(zip(handles, labels), key=lambda x: x[1])
+    handles_sorted, labels_sorted = zip(*handles_labels_sorted)
+    plt.legend(handles_sorted, labels_sorted)
+
     plt.ylim(0, 100)
     plt.ylabel("Predicted LDDT")
     plt.xlabel("Positions")
@@ -52,10 +99,14 @@ def generate_output_images(feature_dict, out_dir, name, pae_plddt_per_model):
     ##################################################################
     num_models = 5 # columns
     num_runs_per_model = math.ceil(len(model_names)/num_models)
-    fig = plt.figure(figsize=(3 * num_models, 2 * num_runs_per_model), dpi=100)
+    fig = plt.figure(figsize=(3 * num_models, 2.3 * num_runs_per_model), dpi=300)
     for n, (model_name, value) in enumerate(pae_plddt_per_model.items()):
         plt.subplot(num_runs_per_model, num_models, n + 1)
-        plt.title(model_name)
+        # plt.title(model_name)
+
+        ranked_file_name = json_rankings[model_name.replace('.pkl', '')]
+        plt.title(f"{ranked_file_name} ({model_name.replace('_ptm.pkl', '')}; \u0078\u0304={avg_plddts[ranked_file_name]})")
+
         plt.imshow(value["pae"], label=model_name, cmap="bwr", vmin=0, vmax=30)
         plt.colorbar()
     fig.tight_layout()
@@ -70,7 +121,8 @@ parser.add_argument('--output_dir',dest='output_dir')
 parser.set_defaults(output_dir='')
 args = parser.parse_args()
 
-feature_dict = pickle.load(open(f'{args.input_dir}/features.pkl','rb'))
+with open(f'{args.input_dir}/features.pkl','rb') as features_pkl:
+    feature_dict = pickle.load(features_pkl)
 # is_multimer = ('result_model_1_multimer.pkl' in [os.path.basename(f) for f in os.listdir(path=args.input_dir)])
 # is_ptm = ('result_model_1_ptm.pkl' in [os.path.basename(f) for f in os.listdir(path=args.input_dir)])
 # model_names = [f'{args.input_dir}/result_model_{f}{"_multimer" if is_multimer else "_ptm" if is_ptm else ""}.pkl' for f in range(1,6)]
